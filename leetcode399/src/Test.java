0323f9d90a3045d6e11e4030c5e28a578c4e17c7
Josvin7 <17868876191@163.com>
2021-01-06 22:27:53 +0800

    leetcode399

---
+++ leetcode399/src/Test.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+/**
+ * @ClassName Test
+ * @Description ：TODO
+ * @Author Josvin
+ * @Date 2021/01/06/10:41
+ */
+public class Test {
+
+//    输入：equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+//    输出：[6.00000,0.50000,-1.00000,1.00000,-1.00000]
+//    解释：
+//    条件：a / b = 2.0, b / c = 3.0
+//    问题：a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+//    结果：[6.0, 0.5, -1.0, 1.0, -1.0 ]
+
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+
+        return values;
+
+    }
+}
