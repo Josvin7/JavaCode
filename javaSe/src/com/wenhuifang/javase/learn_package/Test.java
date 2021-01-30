d56be4c60daabb5da7e5d2487bd2842c8e7d4d41
Josvin7 <17868876191@163.com>
2021-01-30 22:57:02 +0800

    接口

---
+++ javaSe/src/com/wenhuifang/javase/learn_package/Test.java
@@ -0,0 +1,29 @@
+package com.wenhuifang.javase.learn_package;
+
+/**
+ * @ClassName Test
+ * @Description ：TODO
+ * @Author Josvin
+ * @Date 2021/01/30/13:06
+ */
+
+/*
+* final 关键字：
+* 1、修饰变量（属性、静态属性、变量）
+*  该变量只允许有一次复制机会
+*  2、修饰类
+*  该类不能被继承，该类不能有子类
+*  3、修饰类中的普通方法
+*  这个类子类中，不能重写被Final 修饰的方法
+*
+*
+*  构造方法不能被继承
+*
+*
+*
+* */
+
+
+
+public class Test {
+}
