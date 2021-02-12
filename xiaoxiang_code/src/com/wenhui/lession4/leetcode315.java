004171014e4e1f7134d2dbee603009ea56ab92e6
Josvin7 <17868876191@163.com>
2021-02-13 00:16:54 +0800

    315

--- xiaoxiang_code/src/com/wenhui/lession4/leetcode315.java
+++ xiaoxiang_code/src/com/wenhui/lession4/leetcode315.java
@@ -1,6 +1,6 @@
 package com.wenhui.lession4;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @ClassName leetcode315
@@ -10,6 +10,55 @@ import java.util.List;
  */
 public class leetcode315 {
     public List<Integer> countSmaller(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] count = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        merge_sort(map, nums, count);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < count.length; i++) {
+            list.add(count[i]);
+        }
+        return list;
+    }
+    public static void merge_sort_two_array(Map<Integer, Integer> map, int[] array1, int[] array2, int[] array3, int[] count) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] <= array2[j]) {
+                count[map.get(array1[i])] += j;
+                array3[k] = array1[i];
+                i++;
+                k++;
+            } else {
+                array3[k] = array2[j];
+                j++;
+                k++;
+            }
+        }
+        for (; i < array1.length; i++) {
+            count[map.get(array1[i])] += j;
+            array3[k] = array1[i];
+            k++;
+        }
+        for (; j < array2.length; j++) {
+            array3[k] = array2[j];
+            k++;
+        }
 
+    }
+
+    public static void merge_sort(Map<Integer, Integer> map, int[] array, int[] count) {
+        if (array.length < 2) {
+            return;
+        }
+        int mid = array.length / 2;
+        int[] left_array = Arrays.copyOfRange(array, 0, mid);
+        int[] right_array = Arrays.copyOfRange(array, mid, array.length);
+        merge_sort(map, left_array, count);
+        merge_sort(map, right_array, count);
+        merge_sort_two_array(map,left_array,right_array,array,count);
     }
 }
