c41e61b84dcb8a397377e0e5326ab1a80657ef2a
Josvin7 <17868876191@163.com>
2021-02-24 22:37:12 +0800

    字符加法

--- WinterVacationHomework/src/day9/leetcode67.java
+++ WinterVacationHomework/src/day9/leetcode67.java
@@ -7,7 +7,48 @@ package day9;
  * @Date 2021/02/21/14:46
  */
 public class leetcode67 {
+
+
     public static String addBinary(String a, String b) {
+        while(a.length() < b.length()) {
+            a = "0" + a;     //预处理成等长字符串
+        }
+        while(b.length() < a.length()) {
+            b = "0" + b;     //预处理成等长字符串
+        }
+        StringBuilder s1 = new StringBuilder(a);
+        StringBuilder s2 = new StringBuilder(b);
+        StringBuilder r = new StringBuilder();
+        int flag =0;
+        //用flag标记借位
+        // 从左往右计算
+        for (int i = s1.length() - 1; i >= 0; --i) {
+            int sum = f(s1.charAt(i)) + flag + f(s2.charAt(i));
+            flag = 0;
+            r.append(sum % 2);
+            if (sum > 1) {
+                flag = 1;
+            }
+        }
+        if (flag == 1) {
+            r.append(1);
+        }
+        String str =  r.reverse().toString();// 注意要反转
+        // 注意输出 0000 前导0的情况
+        int len = str.length();
+        int index = 0;
+        while( index < len && str.charAt(index) == '0' ) {
+            index++;
+        }
+        if (index == len) {
+            return "0";
+        }else {
+            return str.substring(index, len);
+        }
+    }
+    public static int f(char c) { return c - '0';}     //字符变数字
+
+        public static String addBinary1(String a, String b) {
         char[] charsa = a.toCharArray();
         char[] charsb = b.toCharArray();
         int i = charsa.length - 1;
