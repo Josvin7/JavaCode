706aefa747d922cedbe5a8364f8e87ffe23b2e73
Josvin7 <17868876191@163.com>
2021-03-05 22:40:26 +0800

    顺序表

---
+++ javaDS/src/javaDS0305.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * @ClassName javaDS0305
+ * @Description ：TODO
+ * @Author Josvin
+ * @Date 2021/03/05/21:36
+ */
+public class javaDS0305 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = null;
+        ArrayList<Integer> list1 = new ArrayList<>();
+        System.out.println(list1.isEmpty());
+        System.out.println(list1.size() == 0);
+    }
+}
---
+++ xiaoxiang_code/src/com/wenhui/cvte/test.java
@@ -0,0 +1,11 @@
+package com.wenhui.cvte;
+
+/**
+ * @ClassName test
+ * @Description ：TODO
+ * @Author Josvin
+ * @Date 2021/02/27/18:32
+ */
+public class test {
+
+}
